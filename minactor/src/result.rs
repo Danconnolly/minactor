5b4c33f4f5e70ac14dbae077a0c3b12af0530a90
Daniel Connolly <daniel@dconnolly.com>
2024-09-13 06:15:47 +0000

    unrecognizedmessage error

--- minactor/src/result.rs
+++ minactor/src/result.rs
@@ -7,6 +7,8 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// Standard error type used in the library
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
+    /// An unrecognized message was received.
+    UnrecognizedMessage,
     /// The handler was not implemented, for example a message was sent with no handler defined.
     HandlerNotImplemented,
     /// Unable to send a message, probably due to actor termination.
